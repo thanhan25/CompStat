5bfc0e169701b1e83e9abe7794c9a214c9247086
thanhan25 <44378354+thanhan25@users.noreply.github.com>
2020-05-29 17:39:56 +0200

    Add Interpretation and Marginal Effects

--- PS3.R
+++ PS3.R
@@ -24,9 +24,10 @@ data.gen <- function(n, beta, X1.min, X1.max, X2.P1) {
   X2 <- rbinom(n, 1, X2.P1)
   X <- cbind(X0, X1, X2)
   y <- rep(0, n)
+  logodds <- X %*% beta
   pi_x <- 1 / (1 + exp(-(X %*% beta)))
   y <- rbinom(n, 1, prob = pi_x)
-  data <- cbind.data.frame(X, y)
+  data <- cbind.data.frame(X, logodds, pi_x, y)
   return(data)
 }
 
@@ -39,7 +40,7 @@ X.train <- cbind(data.train$X0, data.train$X1, data.train$X2)
 head(X.train)
 
 # Generate the test data
-data.test <- data.gen(n, beta, X1.min, X1.max, X2.P1) 
+data.test <- data.gen(n, beta, X1.min, X1.max, X2.P1)
 head(data.test)
 
 X.test <- cbind(data.test$X0, data.test$X1, data.test$X2)
@@ -47,20 +48,20 @@ head(X.test)
 
 # Explore the data
 # Scatterplot of ys on X1
-ggplot(data.train, aes(x = X1, y = y, colour = y)) + 
+ggplot(data.train, aes(x = X1, y = y, colour = y)) +
   geom_point()
 
 # ... ys on X2
-ggplot(data.train, aes(x = X2, y = y, colour = y)) + 
+ggplot(data.train, aes(x = X2, y = y, colour = y)) +
   geom_point()
 
 # Distribution of X1
-ggplot(data.train, aes(X1)) + 
+ggplot(data.train, aes(X1)) +
   geom_histogram()
 
 # Distribution of X2
-ggplot(data.train, aes(X2)) + 
-  geom_histogram() 
+ggplot(data.train, aes(X2)) +
+  geom_histogram()
 
 # b)
 # Estimate betas via ML (Logistic Regression)
@@ -70,36 +71,35 @@ summary(Logit)
 
 # Compared with manually doing ML
 
-loglike<-function(b) # the likelihood function for the logit model
-{
-  ll <- sum(-data.train$y * log(1 + exp(-(X.train %*% b))) - 
+loglike<-function(b) {# the likelihood function for the logit model
+  ll <- sum(-data.train$y * log(1 + exp(-(X.train %*% b))) -
               (1 - data.train$y) * log(1 + exp(X.train %*% b)))
   return(ll)
 }
 
 # Initialize estimation procedure
-estim <- maxBFGS(loglike, finalHessian = TRUE, start = c(0, 0, 1)) 
+estim <- maxBFGS(loglike, finalHessian = TRUE, start = c(0, 0, 1))
 beta.ML <- estim$estimate # give out parameter estimates
 beta.ML
 (beta.logit <- Logit$coefficients)
 
 # Standard Error of the Coefficients
-estim.hess <- estim$hessian 
+estim.hess <- estim$hessian
 # the optimization routine returns the hessian matrix at the last iteration.
 Cov <- -(solve(estim.hess))
 # the covariance matrix is the (negative) inverse of the hessian matrix.
-sde <- sqrt(diag(Cov))#the standard errors are the square root of the diagonal of the inverse Hessian. 
+sde <- sqrt(diag(Cov))#the standard errors are the square root of the diagonal of the inverse Hessian.
 sde
 stdEr(Logit)
 
 # Prepare some Data for plotting later
 # "True" log odds and probs (never observe in real life)
-logodds.true.train <- X.train %*% beta 
-probs.true.train <- 1 / (1 + exp(-logodds.true.train))
+logodds.true.train <- data.train$logodds
+probs.true.train <- data.train$pi_x
 
 # "True" log odds and probs for test data
-logodds.true.test <- X.test %*% beta 
-probs.true.test <- 1 / (1 + exp(-logodds.true.test))
+logodds.true.test <- data.test$logodds
+probs.true.test <- data.test$pi_x
 
 # Construct 95% CI for the estimated probs
 # Fit the model again to the data.train
@@ -108,9 +108,10 @@ logodds.fit.train <- logit.train$fit
 probs.fit.train <- 1 / (1 + exp(-logodds.fit.train))
 lower.train <- logodds.fit.train - 1.96 * logit.train$se.fit # lower bound
 upper.train <- logodds.fit.train + 1.96 * logit.train$se.fit # upper bound
-lower.train <- 1 / (1 + exp(-lower.train)) 
+lower.train <- 1 / (1 + exp(-lower.train))
 upper.train <- 1 / (1 + exp(-upper.train))
-
+# Estimated marginal effect of X1
+dx1.train <- beta.logit[2] * probs.fit.train * (1 - probs.fit.train)
 # Use the estimated betas to fit the test data
 logit.test <- predict(Logit, newdata = data.test, se = T)
 logodds.fit.test <- logit.test$fit
@@ -122,7 +123,7 @@ head(X.test %*% beta.logit)
 lower.test <- logodds.fit.test - 1.96 * logit.test$se.fit # lower bound of log odds
 upper.test <- logodds.fit.test + 1.96 * logit.test$se.fit # upper bound...
 (probs.fit.test <- 1 / (1 + exp(-logodds.fit.test)))
-lower.test <- 1 / (1 + exp(-lower.test)) 
+lower.test <- 1 / (1 + exp(-lower.test))
 upper.test <- 1 / (1 + exp(-upper.test))
 
 # MSE and AVE
@@ -159,7 +160,29 @@ addmargins(prop.table(cfm.train, 2))
 addmargins(prop.table(cfm.test))
 addmargins(prop.table(cfm.test, 2))
 
-# c)
+# c) Interpretation
+
+# Logodss of the first observation and P(Y_1 = 1)
+logit.train$fit[1] # "automatic"
+X.train[1, ] %*% beta.logit # "manually"
+# Odss of the first obs
+(odds_1 <- exp(logit.train$fit[1]))
+# With an odds of 1.7:1 the first observation is in group 1
+# Or in other words, the probability that Y_1 = 1 is:
+(pi_x_1 <- 1 / (1 + exp(-logit.train$fit[1]))) # P(Y_1 = 1) is around 64%
+                                               # gives an odds of ~ 64/36 ~ 1.7)
+# Marginal effect:
+# one unit increase in beta_1 increases the odds by e^beta_1 - 1 %
+(exp(beta.logit[2]) - 1) * 100
+logodds_1_new <- beta.logit[1] * X.train[1, 1] +
+beta.logit[2] * (X.train[1, 2] + 1) +
+beta.logit[3] * X.train[1, 3]
+odds_1_new <- exp(logodds_1_new)
+(odds_1_new - odds_1) / odds_1 *100
+
+(dx1_1 <- beta.logit[2] * pi_x_1 * (1 - pi_x_1)) * 100
+pi_x_1_new <- 1 / (1 + exp(-logodds_1_new))
+(pi_x_1_new - pi_x_1) * 100
 
 # d)
 plt.data <- data.frame(X1.train = data.train$X1,
@@ -170,31 +193,37 @@ plt.data <- data.frame(X1.train = data.train$X1,
                        y.test = data.test$y,
                        probs.train = probs.fit.train,
                        probs.test = probs.fit.test,
-                       probs.true.train, 
+                       probs.true.train,
                        probs.true.test,
                        upper.train,
                        lower.train,
                        upper.test,
-                       lower.test) 
+                       lower.test,
+                       dx1.train)
 
-head(plt.data)  
+head(plt.data)
 str(plt.data)
 
 ggplot(plt.data, aes(X1.train)) +
-  geom_line(aes(y = probs.fit.train, colour = X2.train), size = 2) + 
-  geom_ribbon(aes(ymin = lower.train, 
-                  ymax = upper.train, 
-                  colour = X2.train), 
+  geom_line(aes(y = probs.fit.train, colour = X2.train), cex = 1) +
+  geom_ribbon(aes(ymin = lower.train,
+                  ymax = upper.train,
+                  colour = X2.train),
               alpha = 0.5) +
-  geom_point(aes(y = y.train)) + 
-  geom_point(aes(y = probs.true.train), size = 0.1)
+  geom_point(aes(y = y.train), size = 0.3) +
+  geom_point(aes(y = probs.true.train), shape = "x", size = 1.5)
 
-ggplot(plt.data, aes(X1.test)) +
-  geom_line(aes(y = probs.fit.test, colour = X2.test), size = 2) + 
-  geom_ribbon(aes(ymin = lower.test, ymax = upper.test, colour = X2.test), alpha = 0.5) +
-  geom_point(aes(y = y.test)) + 
-  geom_point(aes(y = probs.true.test), size = 0.1)
-
+  ggplot(plt.data, aes(X1.test)) +
+    geom_line(aes(y = probs.fit.test, colour = X2.test), cex = 1) +
+    geom_ribbon(aes(ymin = lower.test,
+                    ymax = upper.test,
+                    colour = X2.test),
+                alpha = 0.5) +
+    geom_point(aes(y = y.test), size = 0.3) +
+    geom_point(aes(y = probs.true.test), shape = "x", size = 1.5)
+# Plot the marginal effect of X1
+ggplot(plt.data, aes(X1.train)) +
+  geom_line(aes(y = dx1.train, colour = X2.train), cex = 1)
 
 ###############################################################
 
@@ -204,15 +233,3 @@ ggplot(plt.data, aes(X1.test)) +
 # MSE and AVE
 
 # change the lower bound of unif can lead to a more "full" logistic curve
-
-
-
-
-
-
-
-
-
-
-
-
